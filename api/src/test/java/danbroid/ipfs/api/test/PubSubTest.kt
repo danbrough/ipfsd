d577882559d1c6955040e68428c75ea077e69bc1
Dan Brough <dan@danbrough.org>
2020-12-18 12:00:38 +1300

    update

--- api/src/test/java/danbroid/ipfs/api/test/PubSubTest.kt
+++ api/src/test/java/danbroid/ipfs/api/test/PubSubTest.kt
@@ -2,11 +2,10 @@ package danbroid.ipfs.api.test
 
 import danbroid.ipfs.api.blocking
 import danbroid.ipfs.api.flow
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.supervisorScope
 import okhttp3.OkHttpClient
 import org.junit.Test
 import java.util.logging.Level
@@ -42,6 +41,52 @@ class PubSubTest {
       }
     }
   }
+
+  @Test
+  fun test2() {
+    val topic = "test"
+    ipfs.blocking {
+      log.warn("starting job")
+
+      val job = launch {
+        var running = true
+        while (running) {
+          runCatching {
+            log.debug("subscribing to $topic")
+            pubsub.sub(topic).invoke().use { response ->
+              response.flow().catch { log.trace("an error happened: ${it.message}") }.collect {
+                log.info("msg: ${it} data:${it.dataString}")
+                if (it.dataString == "stop") {
+                  log.debug("received stop")
+                  running = false
+                  cancel("Received stop")
+                  yield()
+                }
+              }
+              log.info("at this bit")
+            }
+          }.exceptionOrNull()?.also {
+            if (running) {
+              log.debug("something bad happened: \"$it\" reconnecting in 1 second")
+              delay(1000)
+            }
+          }
+        }
+      }
+
+      job.invokeOnCompletion {
+        log.debug("job done: \"${it?.message}\"")
+      }
+
+
+
+
+      log.info("waiting on job")
+      job.join()
+      log.info("THE END")
+    }
+  }
+
 }
 
 
