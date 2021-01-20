1a3ef06ed9a16d075cb49b02e5d29d77ce48ffc6
Dan Brough <dan@danbrough.org>
2021-01-21 07:34:12 +1300

    update

---
+++ scripts/publishBridge.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+PKG_DIR="com/github/danbrough/ipfsd/"
+REPO_SRV="dan@h1"
+REPO_PATH="/srv/https/maven"
+
+cd `dirname $0` && cd ..
+
+./gradlew :bridge:publishReleasePublicationToMavenLocal || exit 1
+
+cd ~/.m2/repository/
+cd $PKG_DIR
+
+find -type f -name maven-metadata-local.xml  | while read n; do
+  mv "$n" $(echo "$n" | sed -e 's:maven-metadata-local:maven-metadata:g')
+done
+
+ssh "$REPO_SRV" mkdir -p "$REPO_PATH/$PKG_DIR"
+rsync -avHSx   . "$REPO_SRV:$REPO_PATH/$PKG_DIR"
+
